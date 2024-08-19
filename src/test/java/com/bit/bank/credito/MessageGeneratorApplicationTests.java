902efeac9bcd4fef1f5bc1841767bfaa630b6f19
Eduardo Carneiro de Assis Silva <eduardoassis52@hotmail.com>
2024-08-19 10:20:37 -0300

    Initial Commit

---
+++ src/test/java/com/bit/bank/credito/MessageGeneratorApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bit.bank.credito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Hello World!");
+	}
+
+}
